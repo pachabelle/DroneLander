db422d78999e5ffe9575387ac1f3f9bf32c57238
Cheng Lim <Cheng.Lim@enghouse.com>
2017-06-16 14:12:19 +1200

    Add project files.

---
+++ DroneLander/DroneLander.Android/Effects/DigitalFontEffect.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using Android.Graphics;
+using Android.Widget;
+using DroneLander.Droid.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ResolutionGroupName("Xamarin")]
+[assembly: ExportEffect(typeof(DigitalFontEffect), "FontEffect")]
+namespace DroneLander.Droid.Effects
+{
+    public class DigitalFontEffect : PlatformEffect
+    {
+        TextView control;
+        protected override void OnAttached()
+        {
+            try
+            {
+                control = Control as TextView;
+                Typeface font = Typeface.CreateFromAsset(Forms.Context.Assets, "Fonts/" + DroneLander.Effects.DigitalFontEffect.GetFontFileName(Element) + ".ttf");
+                control.Typeface = font;
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        protected override void OnDetached()
+        {
+        }
+
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == DroneLander.Effects.DigitalFontEffect.FontFileNameProperty.PropertyName)
+            {
+                Typeface font = Typeface.CreateFromAsset(Forms.Context.ApplicationContext.Assets, "Fonts/" + DroneLander.Effects.DigitalFontEffect.GetFontFileName(Element) + ".ttf");
+                control.Typeface = font;
+            }
+        }
+    }
+}
---
+++ DroneLander/DroneLander/Models/LandingParameters.cs
@@ -0,0 +1,21 @@
+﻿using DroneLander.Common;
+
+namespace DroneLander.Models
+{
+    public enum LandingResultType { Landed, Kaboom, }
+    public class LandingParameters
+    {
+        public LandingParameters()
+        {
+            this.Altitude = CoreConstants.StartingAltitude;
+            this.Velocity = CoreConstants.StartingVelocity;
+            this.Fuel = CoreConstants.StartingFuel;
+            this.Thrust = CoreConstants.StartingThrust;
+        }
+
+        public double Altitude;
+        public double Velocity;
+        public double Fuel;
+        public double Thrust;
+    }
+}
